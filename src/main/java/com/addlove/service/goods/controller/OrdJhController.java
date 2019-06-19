77daa5dcef75ffc6180070e927d415cf89b9d158
lengwen <lengwen@hisense.com>
2019-06-19 17:46:57 +0800

    modify

--- src/main/java/com/addlove/service/goods/controller/OrdJhController.java
+++ src/main/java/com/addlove/service/goods/controller/OrdJhController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.addlove.service.goods.constants.GoodsCommonConstants.BillType;
 import com.addlove.service.goods.constants.GoodsOrdJhConstants.DataStatus;
 import com.addlove.service.goods.constants.GoodsOrdJhConstants.ModelTags;
