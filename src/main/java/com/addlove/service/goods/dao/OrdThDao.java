4d941a8eab62039296266d9a36e192839f662fdc
lengwen <lengwen@hisense.com>
2019-07-11 17:42:48 +0800

    门店退货

---
+++ src/main/java/com/addlove/service/goods/dao/OrdThDao.java
@@ -0,0 +1,13 @@
+package com.addlove.service.goods.dao;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * 门店退货dao层
+ * @author lw
+ *
+ */
+@Repository
+public interface OrdThDao {
+    
+}
