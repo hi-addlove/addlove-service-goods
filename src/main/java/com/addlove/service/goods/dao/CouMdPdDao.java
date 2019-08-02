945fb4230b49a4602c9c142e3404087fba3da76c
lengwen <lengwen@hisense.com>
2019-08-02 13:00:06 +0800

    add pd

---
+++ src/main/java/com/addlove/service/goods/dao/CouMdPdDao.java
@@ -0,0 +1,13 @@
+package com.addlove.service.goods.dao;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * 盘点dao层
+ * @author lw
+ *
+ */
+@Repository
+public interface CouMdPdDao {
+    
+}
---
+++ src/main/java/com/addlove/service/goods/dao/SkuPdCSDao.java
@@ -0,0 +1,13 @@
+package com.addlove.service.goods.dao;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * 商品盘点周期方案dao层
+ * @author lw
+ *
+ */
+@Repository
+public interface SkuPdCSDao {
+    
+}
